28a820f494f15200afd75ae9174bd9ec368b4999
JefferyWang <admin@wangjunfeng.com>
2024-04-21 12:17:17 +0800

    add yaml support

--- src/opts.rs
+++ src/opts.rs
@@ -1,7 +1,10 @@
-use std::path::Path;
+use std::{
+    fmt::{self, Display, Formatter},
+    path::Path,
+    str::FromStr,
+};
 
 use clap::{Parser, Subcommand};
-use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
 #[command(name = "rcli", version, author, about, long_about=None)]
@@ -16,13 +19,22 @@ pub enum SubCommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file, help = "输入 CSV 文件")]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json", help = "输出文件")]
-    pub output: String,
+    #[arg(short, long, help = "输出文件")]
+    pub output: Option<String>,
+
+    #[arg(short, long, value_parser = parse_format, default_value = "json", help = "输出文件格式")]
+    pub format: OutputFormat,
 
     #[arg(short, long, default_value_t = ',', help = "CSV 文件的分隔符")]
     pub delimiter: char,
@@ -39,14 +51,33 @@ fn verify_input_file(filename: &str) -> Result<String, String> {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => anyhow::bail!("Invalid format: {}", s),
+        }
+    }
+}
+
+impl Display for OutputFormat {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
+    }
 }
